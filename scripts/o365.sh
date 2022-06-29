ea9c38e21fd69e0818920a36e0bef5f92982f07c
CG-Tech <support@cg-tech.co.uk>
2022-06-29 07:45:50 +0100

    dev work

--- scripts/o365.sh
+++ scripts/o365.sh
@@ -2,25 +2,25 @@ clear
 read -p "Please enter workspace name: " workspace
 echo $workspace
 echo "Setting email Format"
-EmailFormat=$(cat ~/workspaces/$workspace/Var/emailformat.txt)
+EmailFormat=$(cat $home/workspaces/$workspace/Var/emailformat.txt)
 
 read -p "Do you already have an email list? " -n 1 -r
 echo
-if [[ $REPLY =~ ^[Nn]$ ]]
+if [[ $REPLY =~ ^[Nn]$ ]];
 then
 	
 echo "creating email list"
 read -p "Please enter the full name of the company you would like to Enumerate: " CompName
-python3 /opt/CrossLinked/crosslinked.py -f $EmailFormat  "'$CompName'" -o ~/workspaces/$workspace/Users/emails/EmailList.txt
+python3 /opt/CrossLinked/crosslinked.py -f $EmailFormat  "'$CompName'" -o $home/workspaces/$workspace/Users/emails/EmailList.txt
 echo "email list Complete"
-head -10 ~/workspaces/$workspace/Users/emails/EmailList.txt
+head -10 $home/workspaces/$workspace/Users/emails/EmailList.txt
 
 fi
 
-emaildomain=$(head -1 ~/workspaces/$workspace/Users/emails/EmailList.txt | grep -o "@.*" | sed 's/@//')
+emaildomain=$(head -1 $home/workspaces/$workspace/Users/emails/EmailList.txt | grep -o "@.*" | sed 's/@//')
 read -p "Would you like to Continue to Validation/spray(Active/Bruteforce): " -n 1 -r
 echo    # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
+if [[ ! $REPLY =~ ^[Yy]$ ]];
 then
 	    /opt/AutoRecon/scripts/AutoEnum.sh
 	    exit
@@ -42,14 +42,14 @@ fi
 
 echo "Validating email list"
 
-python3 /opt/o365spray/o365spray.py -d $emaildomain --enum -U ~/workspaces/$workspace/Users/emails/EmailList.txt --output ~/workspaces/$workspace/Users/emails
+python3 /opt/o365spray/o365spray.py -d $emaildomain --enum -U $home/workspaces/$workspace/Users/emails/EmailList.txt --output $home/workspaces/$workspace/Users/emails
 
-validaccountfile=$(ls ~/workspaces/$workspace/Users/emails | grep -E "enum_valid*")
+validaccountfile=$(ls $home/workspaces/$workspace/Users/emails | grep -E "enum_valid*")
 
 echo "setting up password spray" 
 read -p "Please enter a password to use: " password
 
-python3 /opt/o365spray/o365spray.py -d $emaildomain --spray -U ~/workspaces/$workspace/Users/emails/enum/$validaccountfile -p $password --output ~/workspaces/$workspace/Users/emails/spray
+python3 /opt/o365spray/o365spray.py -d $emaildomain --spray -U $home/workspaces/$workspace/Users/emails/enum/$validaccountfile -p $password --output $home/workspaces/$workspace/Users/emails/spray
 
 echo "Password spray finished and saved to: $workspace/Users/emails/spray"
 echo "returning to main menu" 
--- scripts/workspace.sh
+++ scripts/workspace.sh
@@ -1,19 +1,19 @@
 echo "Setting up workspace"
 ##################################################
-
+home=$(echo ~)
 echo "Checking for Workspace folder"
-if [ ! -f ~/workspaces]
+if [ ! -f $home/workspaces];
 then 
-	mkdir ~/workspaces
+	mkdir $home/workspaces
 else 
 	echo "workspaces folder found"
 fi
 
 echo "Available workspaces"
-ls ~/workspaces
+ls $home/workspaces
 read -p "enter Company name: " COMPANYNAME
 
-if [ -f ~/workspaces/$COMPANYNAME ]; then 
+if [ -f $home/workspaces/$COMPANYNAME ]; then 
 	echo "Workspace exists Going to Main Menu"
 	/opt/AutoRecon/scripts/AutoEnum.sh
 	exit 0
@@ -23,27 +23,27 @@ fi
 
 echo "making Working Directories"
 echo "creating Main Working directory"
-mkdir ~/workspaces/$COMPANYNAME
-cd ~/workspaces/$COMPANYNAME 
+mkdir $home/workspaces/$COMPANYNAME
+cd $home/workspaces/$COMPANYNAME 
 echo "creating NMAP folder"
 mkdir NMAP
 echo "creating User account folder"
-mkdir ~/workspaces/$COMPANYNAME/Users
-mkdir ~/workspaces/$COMPANYNAME/Users/emails
-mkdir ~/workspaces/$COMPANYNAME/Users/Passwords
-mkdir ~/workspaces/$COMPANYNAME/Users/ADAccounts
-mkdir ~/workspaces/$COMPANYNAME/Users/emails/spray
+mkdir $home/workspaces/$COMPANYNAME/Users
+mkdir $home/workspaces/$COMPANYNAME/Users/emails
+mkdir $home/workspaces/$COMPANYNAME/Users/Passwords
+mkdir $home/workspaces/$COMPANYNAME/Users/ADAccounts
+mkdir $home/workspaces/$COMPANYNAME/Users/emails/spray
 echo "Creating Variable files"
-mkdir ~/workspaces/$COMPANYNAME/Var
-touch ~/workspaces/$COMPANYNAME/Var/domain.txt
-touch ~/workspaces/$COMPANYNAME/Var/webaddress.txt
-mkdir ~/workspaces/$COMPANYNAME/Var/tmp
-touch ~/workspaces/$COMPANYNAME/Var/emailformat.txt
-mkdir ~/workspaces/$COMPANYNAME/DNSInfo
+mkdir $home/workspaces/$COMPANYNAME/Var
+touch $home/workspaces/$COMPANYNAME/Var/domain.txt
+touch $home/workspaces/$COMPANYNAME/Var/webaddress.txt
+mkdir $home/workspaces/$COMPANYNAME/Var/tmp
+touch $home/workspaces/$COMPANYNAME/Var/emailformat.txt
+mkdir $home/workspaces/$COMPANYNAME/DNSInfo
 echo "creating notes"
-touch ~/workspaces/$COMPANYNAME/Notes.txt
+touch $home/workspaces/$COMPANYNAME/Notes.txt
 echo "workspace Set up"
-tree ~/workspaces/$COMPANYNAME
+tree $home/workspaces/$COMPANYNAME
 read -n 1 -r -s -p "Press any key to continue" key
 #################################################
 #network details
@@ -51,8 +51,8 @@ read -n 1 -r -s -p "Press any key to continue" key
 clear 
 echo "Network details"
 read -p "please enter external address: " EXTADDR
-mkdir ~/workspaces/$COMPANYNAME/NMAP/External
-echo $EXTADDR > ~/workspaces/$COMPANYNAME/NMAP/External/extTargets.txt
+mkdir $home/workspaces/$COMPANYNAME/NMAP/External
+echo $EXTADDR > $home/workspaces/$COMPANYNAME/NMAP/External/extTargets.txt
 read -n 1 -r -s -p "Press any key to continue" key
 ##################################################
 #user details 
@@ -65,9 +65,9 @@ echo "{f}{l}@domain.com"
 echo "{first}.{last}@domain.com"
 echo "this is using crosslinked for more formats see /opt/crosslinked.py -h" 
 read -p "Email Format:  " emailformat
-echo "$emailformat" >> ~/workspaces/$COMPANYNAME/Var/emailformat.txt
+echo "$emailformat" >> $home/workspaces/$COMPANYNAME/Var/emailformat.txt
 echo "email format created"
-cat ~/workspaces/$COMPANYNAME/Var/emailformat.txt
+cat $home/workspaces/$COMPANYNAME/Var/emailformat.txt
 read -n 1 -r -s -p "if email format looks correct press any key to set domain information" key
 
 ######################
@@ -75,19 +75,19 @@ read -n 1 -r -s -p "if email format looks correct press any key to set domain in
 #######################
 #domain name lookup 
 echo "Finding domain name from $EXTADDR"
-nslookup $EXTADDR > ~/workspaces/$COMPANYNAME/Var/domain.txt
+nslookup $EXTADDR > $home/workspaces/$COMPANYNAME/Var/domain.txt
 #formating nslookup output
-grep 'name' ~/workspaces/$COMPANYNAME/Var/domain.txt > ~/workspaces/$COMPANYNAME/Var/tmp/domain.txt
+grep 'name' $home/workspaces/$COMPANYNAME/Var/domain.txt > $home/workspaces/$COMPANYNAME/Var/tmp/domain.txt
 # selecting only string after =
-cat ~/workspaces/$COMPANYNAME/Var/tmp/domain.txt | cut -d "=" -f2- > ~/workspaces/$COMPANYNAME/Var/domain.txt
+cat $home/workspaces/$COMPANYNAME/Var/tmp/domain.txt | cut -d "=" -f2- > $home/workspaces/$COMPANYNAME/Var/domain.txt
 # removing . after string
-sed '$ s/.$//' ~/workspaces/$COMPANYNAME/Var/domain.txt > ~/workspaces/$COMPANYNAME/Var/tmp/domain.txt
+sed '$ s/.$//' $home/workspaces/$COMPANYNAME/Var/domain.txt > $home/workspaces/$COMPANYNAME/Var/tmp/domain.txt
 #removing whitespaces
-cat ~/workspaces/$COMPANYNAME/Var/tmp/domain.txt | sed 's/ //g' > ~/workspaces/$COMPANYNAME/Var/domain.txt
-cat ~/workspaces/$COMPANYNAME/Var/domain.txt
+cat $home/workspaces/$COMPANYNAME/Var/tmp/domain.txt | sed 's/ //g' > $home/workspaces/$COMPANYNAME/Var/domain.txt
+cat $home/workspaces/$COMPANYNAME/Var/domain.txt
 echo "IP: $EXTADDR"
 echo "Domain:"
-cat ~/workspaces/$COMPANYNAME/Var/domain.txt
+cat $home/workspaces/$COMPANYNAME/Var/domain.txt
 
 ############################
 
